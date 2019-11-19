2173d901ba96d6aaf844d314c72233ca2fcd1550
HP 8470p <Darioponzo03@gmail.com>
2019-11-19 13:05:12 +0100

    push PHP les 3

---
+++ h03/busreis.php
@@ -0,0 +1,31 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<head>
+    <title>PHP Opdrachten</title>
+    <style>
+        
+    </style>
+</head>
+<body>
+<?php
+$leeftijd = 2;
+$bedrag = 10;
+if ($leeftijd > 65){
+    $bedrag =$bedrag * 0.5;
+}
+
+if ($leeftijd <= 12){
+    $bedrag =$bedrag * 0.5;
+}
+
+if ($leeftijd < 3){
+    $bedrag =0;
+}
+
+echo $bedrag;
+
+?>
+</body>
+</html>
