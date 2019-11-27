c735a6689d9e9dfccc3981436ac341ce243bce9b
HP 8470p <Darioponzo03@gmail.com>
2019-11-27 13:21:48 +0100

    push PHP les 8

---
+++ h08/Mr Wheely/index.php
@@ -0,0 +1,48 @@
+<?php
+include_once ("AutoOverzicht.php")
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Mr Wheely</title>
+    <link href="autoStyle.css" rel="stylesheet">
+</head>
+<body>
+<header>
+    <img src="img/wheely_header.jpg" alt="Mr Wheeky logo">
+</header>
+<form role="form" method="post">
+<div class="form">
+    <label for="sell">Merk:</label>
+    <select class="form kiezen" id="merk" name="merk">
+        <option value="">--Alle merken--</option>
+        <option value='Audi' >Audi</option>
+        <option value='Ferrari' >Ferrari</option>
+        <option value='Fiat' >Fiat</option>
+        <option value='Mercedes' >Mercedes</option>
+        <option value='Opel' >Opel</option>
+        <option value='Volkswagen' >Volkswagen</option>
+    </select>
+</div>
+<div class="form">
+    <label for="type">Minimale prijs:</label>
+    <input type="text" class="Form kiezen" name="lage prijz" value="">
+</div>
+<div class="form">
+    <label for="type">Maximale prijs:</label>
+    <input type="text" class="Form kiezen" name="hoge prijz" value="">
+</div>
+<button type="submit" name="knop" class="btn btn-default">Submit</button>
+</form>
+<br>
+<?php
+if ($_POST['merk'] !="") {
+    getGefilterdeLijst();
+}else{
+    VoegAutoToeOverzicht();
+}
+?>
+</body>
+</html>
--- h08/monkeyBusiness/index.php
+++ h08/monkeyBusiness/index.php
@@ -8,10 +8,11 @@ include_once ("apenLijst.php");
     <meta charset="UTF-8">
     <title>Monkey Business</title>
     <link href="style.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Bangers&display=swap" rel="stylesheet">
 </head>
 <body>
 <img src="img/monkey-business.jpg" alt="monkey business logo">
-<h1>Select your monkey!</h1>
+<h1 class="p">Select your monkey!</h1>
 <img src="img/monkey_swings.png" alt="monkey swing"><br>
 <?php
 LijstMaker();
